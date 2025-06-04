299918404799c6659a4f56f373bf4c920ec7b10e
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:45:03 -0300

    Service Producto creado

--- src/main/java/ar/edu/utn/gestion_inventario/service/ProductoService.java
+++ src/main/java/ar/edu/utn/gestion_inventario/service/ProductoService.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.gestion_inventario.service;
 
+import ar.edu.utn.gestion_inventario.repository.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
 public class ProductoService {
+    @Autowired
+    private ProductoRepository productoRepository;
 }
