2cb93ee8679be41a74da731aeaacbd475bc07a80
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 00:01:36 -0300

    Repository Security Usuario creado

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/repository/UsuarioRepository.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/usuario/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package ar.edu.utn.gestion_inventario.security.usuario.repository;
 
-public interface UsuarioRepository {
+import ar.edu.utn.gestion_inventario.security.usuario.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
 }
