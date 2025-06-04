ae22a88736efdcf8952fed3596ae745f8d889b23
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:47:07 -0300

    Controller Descuento creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.gestion_inventario.controller;
 
+import ar.edu.utn.gestion_inventario.service.DescuentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/descuento")
 public class DescuentoController {
+    @Autowired
+    private DescuentoService descuentoService;
 }
