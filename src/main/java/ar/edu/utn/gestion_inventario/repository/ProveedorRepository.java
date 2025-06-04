96509496680b8fece331b256b2c54d43a89f98f1
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 16:10:08 -0300

    Metodo crearProducto (service) creado

--- src/main/java/ar/edu/utn/gestion_inventario/repository/ProveedorRepository.java
+++ src/main/java/ar/edu/utn/gestion_inventario/repository/ProveedorRepository.java
@@ -4,4 +4,5 @@ import ar.edu.utn.gestion_inventario.model.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
+    Proveedor getReferenceByEmail(String email);
 }
--- src/main/java/ar/edu/utn/gestion_inventario/service/ProductoService.java
+++ src/main/java/ar/edu/utn/gestion_inventario/service/ProductoService.java
@@ -1,6 +1,13 @@
 package ar.edu.utn.gestion_inventario.service;
 
+import ar.edu.utn.gestion_inventario.dto.producto.ProductoDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.producto.ProductoRequestDTO;
+import ar.edu.utn.gestion_inventario.model.Descuento;
+import ar.edu.utn.gestion_inventario.model.Producto;
+import ar.edu.utn.gestion_inventario.model.Proveedor;
+import ar.edu.utn.gestion_inventario.repository.DescuentoRepository;
 import ar.edu.utn.gestion_inventario.repository.ProductoRepository;
+import ar.edu.utn.gestion_inventario.repository.ProveedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -10,4 +17,18 @@ import org.springframework.validation.annotation.Validated;
 public class ProductoService {
     @Autowired
     private ProductoRepository productoRepository;
+
+    @Autowired
+    private DescuentoRepository descuentoRepository;
+
+    @Autowired
+    private ProveedorRepository proveedorRepository;
+
+    public ProductoDetailDTO crearProducto(ProductoRequestDTO dto)
+    {
+        Descuento descuento = descuentoRepository.getReferenceById(dto.getIdDescuento());
+        Proveedor proveedor = proveedorRepository.getReferenceByEmail(dto.getEmailProveedor());
+        Producto producto = productoRepository.save(new Producto(dto.getNombre(), dto.getDescripcion(), dto.getCategoria(), dto.getPrecioUnitario(), dto.getCodigoBarras(), proveedor, descuento));
+        return new ProductoDetailDTO(producto.getId(), producto.getNombre(), producto.getDescripcion(), producto.getCategoria(), producto.getPrecioUnitario(), producto.getCodigoBarras());
+    }
 }
