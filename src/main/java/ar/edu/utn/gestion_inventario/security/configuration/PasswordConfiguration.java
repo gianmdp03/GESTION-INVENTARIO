9394b595a654e78126bc17cea189b789d1843639
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:55:51 -0300

    PasswordEncoder agregado

--- src/main/java/ar/edu/utn/gestion_inventario/security/configuration/PasswordConfiguration.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/configuration/PasswordConfiguration.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.gestion_inventario.security.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class PasswordConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 }
