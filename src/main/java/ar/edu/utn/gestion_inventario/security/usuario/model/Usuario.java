e84b8d1bcd8f5befe8d56b7d0bce8e3afe126636
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 00:00:58 -0300

    Entidad Security Usuario creada

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/model/Usuario.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/usuario/model/Usuario.java
@@ -1,4 +1,82 @@
 package ar.edu.utn.gestion_inventario.security.usuario.model;
 
-public class Usuario {
+import ar.edu.utn.gestion_inventario.ENUM.TipoUsuario;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String username;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private TipoUsuario tipoUsuario; // ADMINISTRADOR, EMPLEADO
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + tipoUsuario.name()));
+    }
+    public Usuario() {
+    }
+
+    public Usuario(Long id) {
+        this.id = id;
+    }
+
+    public Usuario(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Usuario(String username, String password, TipoUsuario tipoUsuario) {
+        this.username = username;
+        this.password = password;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setRol(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
+
