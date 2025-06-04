e7746e3d75ad0619243fa1a3a6834efaf26296ff
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:20:25 -0300

    DTO DetalleVentaList creado

--- src/main/java/ar/edu/utn/gestion_inventario/dto/detalleventa/DetalleVentaListDTO.java
+++ src/main/java/ar/edu/utn/gestion_inventario/dto/detalleventa/DetalleVentaListDTO.java
@@ -1,4 +1,5 @@
 package ar.edu.utn.gestion_inventario.dto.detalleventa;
 
-public record DetalleVentaListDTO() {
-}
+import java.math.BigDecimal;
+
+public record DetalleVentaListDTO (Long id, int cantidad, BigDecimal precioUnitario, Long idVenta, String nombreProducto) {}
