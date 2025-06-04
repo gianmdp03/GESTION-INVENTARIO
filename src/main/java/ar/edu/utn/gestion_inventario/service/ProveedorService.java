44e89bf63f27ce0dd7d79b237c9c70133f8e0899
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 15:39:22 -0300

    Metodo listarProveedores (service) creado

--- src/main/java/ar/edu/utn/gestion_inventario/service/ProveedorService.java
+++ src/main/java/ar/edu/utn/gestion_inventario/service/ProveedorService.java
@@ -1,12 +1,15 @@
 package ar.edu.utn.gestion_inventario.service;
 
 import ar.edu.utn.gestion_inventario.dto.proveedor.ProveedorDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.proveedor.ProveedorListDTO;
 import ar.edu.utn.gestion_inventario.dto.proveedor.ProveedorRequestDTO;
 import ar.edu.utn.gestion_inventario.model.Proveedor;
 import ar.edu.utn.gestion_inventario.repository.ProveedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
 
 @Service
 @Validated
@@ -18,5 +21,11 @@ public class ProveedorService {
     {
         Proveedor proveedor = proveedorRepository.save(new Proveedor(dto.getNombre(), dto.getTelefono(), dto.getEmail(), dto.getDireccion()));
         return new ProveedorDetailDTO(proveedor.getId(), proveedor.getNombre(), proveedor.getTelefono(), proveedor.getEmail(), proveedor.getEmail());
+    }
+    public List<ProveedorListDTO> listarProveedores()
+    {
+        List<ProveedorListDTO> dto = proveedorRepository.findAll().stream().map(proveedor -> new ProveedorListDTO(proveedor.getId(),
+                proveedor.getNombre(), proveedor.getTelefono(), proveedor.getEmail())).toList();
+        return dto;
     }
 }
