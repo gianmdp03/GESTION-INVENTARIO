5a0190303d1d0536328c623b058d64a8621134b9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:11:36 -0300

    Entidad Producto creada

--- src/main/java/ar/edu/utn/gestion_inventario/model/Producto.java
+++ src/main/java/ar/edu/utn/gestion_inventario/model/Producto.java
@@ -1,4 +1,131 @@
 package ar.edu.utn.gestion_inventario.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(nullable = false, unique = true)
+    private String codigoBarras;
+
+    @ManyToOne
+    @JoinColumn(name = "proveedor_id")
+    private Proveedor proveedor;
+
+    @ManyToOne
+    @JoinColumn(name = "descuento_id")
+    private Descuento descuento;
+
+    public Producto() {
+    }
+
+    public Producto(Long id) {
+        this.id = id;
+    }
+
+    public Producto(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Producto(String nombre, String descripcion, String categoria, BigDecimal precioUnitario, String codigoBarras, Proveedor proveedor, Descuento descuento) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.precioUnitario = precioUnitario;
+        this.codigoBarras = codigoBarras;
+        this.proveedor = proveedor;
+        this.descuento = descuento;
+    }
+
+    public Producto(String nombre, String descripcion, String categoria, BigDecimal precioUnitario, String codigoBarras, Proveedor proveedor) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria = categoria;
+        this.precioUnitario = precioUnitario;
+        this.codigoBarras = codigoBarras;
+        this.proveedor = proveedor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public String getCodigoBarras() {
+        return codigoBarras;
+    }
+
+    public void setCodigoBarras(String codigoBarras) {
+        this.codigoBarras = codigoBarras;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public Descuento getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(Descuento descuento) {
+        this.descuento = descuento;
+    }
 }
