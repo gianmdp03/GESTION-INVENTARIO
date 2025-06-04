67f07f5b5973b1ae241813988fd872eeb6d92473
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 15:36:03 -0300

    Metodo crearProveedor (service) creado

--- src/main/java/ar/edu/utn/gestion_inventario/service/ProveedorService.java
+++ src/main/java/ar/edu/utn/gestion_inventario/service/ProveedorService.java
@@ -1,5 +1,8 @@
 package ar.edu.utn.gestion_inventario.service;
 
+import ar.edu.utn.gestion_inventario.dto.proveedor.ProveedorDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.proveedor.ProveedorRequestDTO;
+import ar.edu.utn.gestion_inventario.model.Proveedor;
 import ar.edu.utn.gestion_inventario.repository.ProveedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,10 @@ import org.springframework.validation.annotation.Validated;
 public class ProveedorService {
     @Autowired
     private ProveedorRepository proveedorRepository;
+
+    public ProveedorDetailDTO crearProveedor(ProveedorRequestDTO dto)
+    {
+        Proveedor proveedor = proveedorRepository.save(new Proveedor(dto.getNombre(), dto.getTelefono(), dto.getEmail(), dto.getDireccion()));
+        return new ProveedorDetailDTO(proveedor.getId(), proveedor.getNombre(), proveedor.getTelefono(), proveedor.getEmail(), proveedor.getEmail());
+    }
 }
