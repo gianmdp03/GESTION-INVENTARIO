f739d16164e97ded6d6c5db11b59873b0d5ef0e9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:51:49 -0300

    Controller Proveedor creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/ProveedorController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/ProveedorController.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.gestion_inventario.controller;
 
+import ar.edu.utn.gestion_inventario.service.ProveedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proveedor")
 public class ProveedorController {
+    @Autowired
+    private ProveedorService proveedorService;
 }
