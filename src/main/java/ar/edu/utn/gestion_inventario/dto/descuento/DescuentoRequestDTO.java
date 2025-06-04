7fa7f205d592b1c29ceed8939d1d54bee2c96f59
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:18:49 -0300

    DTO DescuentoRequest creado

--- src/main/java/ar/edu/utn/gestion_inventario/dto/descuento/DescuentoRequestDTO.java
+++ src/main/java/ar/edu/utn/gestion_inventario/dto/descuento/DescuentoRequestDTO.java
@@ -1,4 +1,61 @@
 package ar.edu.utn.gestion_inventario.dto.descuento;
 
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class DescuentoRequestDTO {
+    private String descripcion;
+    @NotNull
+    private BigDecimal porcentaje;
+    @NotNull
+    private LocalDate fechaInicio;
+    @NotNull
+    private LocalDate fechaFin;
+
+    public DescuentoRequestDTO(String descripcion, BigDecimal porcentaje, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.descripcion = descripcion;
+        this.porcentaje = porcentaje;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public DescuentoRequestDTO(BigDecimal porcentaje, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.porcentaje = porcentaje;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(BigDecimal porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
 }
