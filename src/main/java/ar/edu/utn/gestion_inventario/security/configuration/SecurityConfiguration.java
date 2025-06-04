3e913c2c114b9a0136e957c83d32a17014a60aeb
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 00:33:02 -0300

    SecurityConfiguration creado (sin implementar)

--- src/main/java/ar/edu/utn/gestion_inventario/security/configuration/SecurityConfiguration.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/configuration/SecurityConfiguration.java
@@ -1,4 +1,37 @@
 package ar.edu.utn.gestion_inventario.security.configuration;
 
+import ar.edu.utn.gestion_inventario.security.usuario.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfiguration {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    /*@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
+    {
+        return httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/pelicula").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/api/pelicula").hasAnyRole("ADMIN", "EMPLEADO")
+                .anyRequest().authenticated()
+        ).userDetailsService(usuarioService).httpBasic(Customizer.withDefaults()).sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }*/
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
