0f339e72303292a5b639698bc0206776bdbbe878
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:07:43 -0300

    Entidad Descuento creada

--- src/main/java/ar/edu/utn/gestion_inventario/model/Descuento.java
+++ src/main/java/ar/edu/utn/gestion_inventario/model/Descuento.java
@@ -1,4 +1,97 @@
 package ar.edu.utn.gestion_inventario.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "descuentos")
 public class Descuento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(nullable = false, precision = 5, scale = 2)
+    private BigDecimal porcentaje;
+
+    @Column(nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(nullable = false)
+    private LocalDate fechaFin;
+
+    @OneToMany(mappedBy = "descuento")
+    private List<Producto> productos = new ArrayList<>();
+
+    public Descuento() {
+    }
+
+    public Descuento(Long id) {
+        this.id = id;
+    }
+
+    public Descuento(String descripcion, BigDecimal porcentaje, LocalDate fechaInicio, LocalDate fechaFin) {
+        this.descripcion = descripcion;
+        this.porcentaje = porcentaje;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(BigDecimal porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
 }
