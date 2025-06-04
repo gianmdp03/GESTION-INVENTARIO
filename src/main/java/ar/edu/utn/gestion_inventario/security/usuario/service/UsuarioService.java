318466d12682a721f7973411c6428304b0ea6e6b
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 00:04:25 -0300

    Service Security Usuario creado

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/service/UsuarioService.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/usuario/service/UsuarioService.java
@@ -1,4 +1,35 @@
 package ar.edu.utn.gestion_inventario.security.usuario.service;
 
-public class UsuarioService {
+import ar.edu.utn.gestion_inventario.ENUM.TipoUsuario;
+import ar.edu.utn.gestion_inventario.security.usuario.model.Usuario;
+import ar.edu.utn.gestion_inventario.security.usuario.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario crearUsuario(Usuario usuario)
+    {
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        if(usuario.getTipoUsuario()==null)
+        {
+            usuario.setRol(TipoUsuario.EMPLEADO);
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        return usuarioRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+    }
 }
