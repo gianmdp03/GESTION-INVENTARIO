9bc57b88c2e3c18368ee7e2fea6e575f8d8d6ad1
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:45:44 -0300

    Service Proveedor creado

--- src/main/java/ar/edu/utn/gestion_inventario/service/ProveedorService.java
+++ src/main/java/ar/edu/utn/gestion_inventario/service/ProveedorService.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.gestion_inventario.service;
 
+import ar.edu.utn.gestion_inventario.repository.ProveedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
 public class ProveedorService {
+    @Autowired
+    private ProveedorRepository proveedorRepository;
 }
