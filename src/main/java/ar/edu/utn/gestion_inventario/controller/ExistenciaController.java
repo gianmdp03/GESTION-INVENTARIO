9e2fd8bfa4d530c65f1f2d683f4fd209cb341bb8
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:50:38 -0300

    Controller Existencia creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/ExistenciaController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/ExistenciaController.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.gestion_inventario.controller;
 
+import ar.edu.utn.gestion_inventario.service.ExistenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/existencia")
 public class ExistenciaController {
+    @Autowired
+    private ExistenciaService existenciaService;
 }
