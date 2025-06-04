8acd044e65fca6618f6764b41cf5bf86eec134bb
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-04 00:18:30 -0300

    Controller Security Usuario creado

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/controller/UsuarioController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/usuario/controller/UsuarioController.java
@@ -1,4 +1,38 @@
 package ar.edu.utn.gestion_inventario.security.usuario.controller;
 
+import ar.edu.utn.gestion_inventario.security.usuario.dto.UsuarioListDTO;
+import ar.edu.utn.gestion_inventario.security.usuario.model.Usuario;
+import ar.edu.utn.gestion_inventario.security.usuario.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/auth")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> crearUsuario(@Valid @RequestBody Usuario usuario)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.crearUsuario(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioListDTO>> listarUsuario()
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioService.listarUsuarios());
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<Void> eliminarPorUsername(@PathVariable String username)
+    {
+        usuarioService.eliminarUsuarioPorUsername(username);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
