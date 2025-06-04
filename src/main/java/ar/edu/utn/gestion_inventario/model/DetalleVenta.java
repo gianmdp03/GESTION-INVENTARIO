526d7979fd432eca1eb2995f96784436ac15a5ac
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:08:43 -0300

    Entidad DetalleVenta Creada

--- src/main/java/ar/edu/utn/gestion_inventario/model/DetalleVenta.java
+++ src/main/java/ar/edu/utn/gestion_inventario/model/DetalleVenta.java
@@ -1,4 +1,82 @@
 package ar.edu.utn.gestion_inventario.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "detalle_ventas")
 public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int cantidad;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @ManyToOne
+    @JoinColumn(name = "venta_id")
+    private Venta venta;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+
+    public DetalleVenta() {
+    }
+
+    public DetalleVenta(Long id) {
+        this.id = id;
+    }
+
+    public DetalleVenta(int cantidad, BigDecimal precioUnitario, Venta venta, Producto producto) {
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.venta = venta;
+        this.producto = producto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
 }
