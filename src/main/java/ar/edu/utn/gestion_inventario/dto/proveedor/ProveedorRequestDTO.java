7d2bde939bb19651c82a47efc16cee81fe77ca99
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-03 23:27:35 -0300

    DTO ProveedorRequest creado

--- src/main/java/ar/edu/utn/gestion_inventario/dto/proveedor/ProveedorRequestDTO.java
+++ src/main/java/ar/edu/utn/gestion_inventario/dto/proveedor/ProveedorRequestDTO.java
@@ -1,4 +1,56 @@
 package ar.edu.utn.gestion_inventario.dto.proveedor;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class ProveedorRequestDTO {
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String telefono;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String direccion;
+
+    public ProveedorRequestDTO(String nombre, String telefono, String email, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
+
