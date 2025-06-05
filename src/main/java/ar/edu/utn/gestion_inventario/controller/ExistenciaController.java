abf5e186fa4e19be4ea1924b0a8ba798bf302d22
Joaquin <joaco.rodriguezortega@gmail.com>
2025-06-05 20:39:26 -0300

    Metodo crearExistencia (controller) creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/ExistenciaController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/ExistenciaController.java
@@ -1,7 +1,12 @@
 package ar.edu.utn.gestion_inventario.controller;
 
+import ar.edu.utn.gestion_inventario.dto.existencia.ExistenciaDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.existencia.ExistenciaRequestDTO;
 import ar.edu.utn.gestion_inventario.service.ExistenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExistenciaController {
     @Autowired
     private ExistenciaService existenciaService;
+
+    @PostMapping
+    public ResponseEntity<ExistenciaDetailDTO> crearExistencia(ExistenciaRequestDTO dto)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(existenciaService.crearExistencia(dto));
+    }
 }
