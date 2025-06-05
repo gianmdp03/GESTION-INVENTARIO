3f1357c0c379511670f187272787224638e52391
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-05 15:57:02 -0300

    Correción a anotaciones faltantes (error crítico)

--- src/main/java/ar/edu/utn/gestion_inventario/repository/ProductoRepository.java
+++ src/main/java/ar/edu/utn/gestion_inventario/repository/ProductoRepository.java
@@ -2,6 +2,8 @@ package ar.edu.utn.gestion_inventario.repository;
 
 import ar.edu.utn.gestion_inventario.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
