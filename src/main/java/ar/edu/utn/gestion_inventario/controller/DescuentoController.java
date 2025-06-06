851d87fc11cfab15cf9708f9327ae6eb24d1811f
Joaquin <joaco.rodriguezortega@gmail.com>
2025-06-05 21:22:58 -0300

    Metodo modificarDescuento (controller) creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
@@ -1,16 +1,16 @@
 package ar.edu.utn.gestion_inventario.controller;
 
 import ar.edu.utn.gestion_inventario.dto.descuento.DescuentoDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.descuento.DescuentoListDTO;
 import ar.edu.utn.gestion_inventario.dto.descuento.DescuentoRequestDTO;
 import ar.edu.utn.gestion_inventario.service.DescuentoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/descuento")
@@ -22,5 +22,11 @@ public class DescuentoController {
     public ResponseEntity<DescuentoDetailDTO> crearDescuento(@Valid @RequestBody DescuentoRequestDTO dto)
     {
         return ResponseEntity.status(HttpStatus.CREATED).body(descuentoService.crearDescuento(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DescuentoDetailDTO> modificarDescuento(@PathVariable Long id, @Valid @RequestBody DescuentoRequestDTO dto)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(descuentoService.modificarDescuento(id, dto));
     }
 }
