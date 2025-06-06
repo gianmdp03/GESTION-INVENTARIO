7d6362ae4ce9897711726c212bb72ab3cd26102e
Joaquin <joaco.rodriguezortega@gmail.com>
2025-06-05 21:23:28 -0300

    Metodo listarDescuentos (controller) creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -28,5 +27,11 @@ public class DescuentoController {
     public ResponseEntity<DescuentoDetailDTO> modificarDescuento(@PathVariable Long id, @Valid @RequestBody DescuentoRequestDTO dto)
     {
         return ResponseEntity.status(HttpStatus.OK).body(descuentoService.modificarDescuento(id, dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DescuentoListDTO>> listarDescuentos()
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(descuentoService.listarDescuentos());
     }
 }
