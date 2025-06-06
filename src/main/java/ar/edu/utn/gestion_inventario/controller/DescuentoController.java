c1ab2a6974e2616296db8ca33896f8a5d779fe9c
Joaquin <joaco.rodriguezortega@gmail.com>
2025-06-05 21:16:56 -0300

    Metodo crearDescuento (controller) creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/DescuentoController.java
@@ -1,7 +1,14 @@
 package ar.edu.utn.gestion_inventario.controller;
 
+import ar.edu.utn.gestion_inventario.dto.descuento.DescuentoDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.descuento.DescuentoRequestDTO;
 import ar.edu.utn.gestion_inventario.service.DescuentoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DescuentoController {
     @Autowired
     private DescuentoService descuentoService;
+
+    @PostMapping
+    public ResponseEntity<DescuentoDetailDTO> crearDescuento(@Valid @RequestBody DescuentoRequestDTO dto)
+    {
+        return ResponseEntity.status(HttpStatus.CREATED).body(descuentoService.crearDescuento(dto));
+    }
 }
