40d76c7898a479829736529b1ad47f0f024b9856
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-05 21:24:03 -0300

    Metodo crearProducto (controller) creado

--- src/main/java/ar/edu/utn/gestion_inventario/controller/ProductoController.java
+++ src/main/java/ar/edu/utn/gestion_inventario/controller/ProductoController.java
@@ -1,13 +1,23 @@
 package ar.edu.utn.gestion_inventario.controller;
 
+import ar.edu.utn.gestion_inventario.dto.producto.ProductoDetailDTO;
+import ar.edu.utn.gestion_inventario.dto.producto.ProductoRequestDTO;
 import ar.edu.utn.gestion_inventario.service.ProductoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/producto")
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
+
+    @PostMapping
+    public ResponseEntity<ProductoDetailDTO> crearProducto(@Valid @RequestBody ProductoRequestDTO dto)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(productoService.crearProducto(dto));
+    }
 }
