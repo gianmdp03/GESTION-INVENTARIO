d91f528fac9b095c27a6a0b9f99d26c6bd54db75
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-09 19:53:14 -0300

    Metodo para borrar por fecha de vencimiento creado (repository)

--- src/main/java/ar/edu/utn/gestion_inventario/repository/ExistenciaRepository.java
+++ src/main/java/ar/edu/utn/gestion_inventario/repository/ExistenciaRepository.java
@@ -4,10 +4,12 @@ import ar.edu.utn.gestion_inventario.model.Existencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface ExistenciaRepository extends JpaRepository<Existencia, Long> {
     List<Existencia> findAllByCantidadLessThanOrderByCantidadAsc(int cantidad);
     List<Existencia> findAllByCantidadGreaterThanOrderByCantidadDesc(int cantidad);
+    void deleteByFechaVencimientoBefore(LocalDate fecha);
 }
