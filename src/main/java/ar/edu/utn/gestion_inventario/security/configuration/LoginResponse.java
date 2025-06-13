0d61c44711e0d561844a74665f9acbc0b78e81f5
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-13 16:55:29 -0300

    AuthenticationController creado

---
+++ src/main/java/ar/edu/utn/gestion_inventario/security/configuration/LoginResponse.java
@@ -0,0 +1,31 @@
+package ar.edu.utn.gestion_inventario.security.configuration;
+
+public class LoginResponse {
+    private String token;
+
+    private Long expiresIn;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String token, Long expiresIn) {
+        this.token = token;
+        this.expiresIn = expiresIn;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
---
+++ src/main/java/ar/edu/utn/gestion_inventario/security/controller/AuthenticationController.java
@@ -0,0 +1,42 @@
+package ar.edu.utn.gestion_inventario.security.controller;
+
+import ar.edu.utn.gestion_inventario.security.configuration.LoginResponse;
+import ar.edu.utn.gestion_inventario.security.dto.UsuarioLoginRequestDTO;
+import ar.edu.utn.gestion_inventario.security.dto.UsuarioRegisterRequestDTO;
+import ar.edu.utn.gestion_inventario.security.model.Usuario;
+import ar.edu.utn.gestion_inventario.security.service.AuthenticationService;
+import ar.edu.utn.gestion_inventario.security.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/auth")
+@RestController
+public class AuthenticationController {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Usuario> register(@RequestBody UsuarioRegisterRequestDTO dto) {
+        Usuario usuarioRegistrado = authenticationService.iniciarSesion(dto);
+
+        return ResponseEntity.ok(usuarioRegistrado);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody UsuarioLoginRequestDTO dto) {
+        Usuario authenticatedUser = authenticationService.autenticar(dto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getJwtExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
