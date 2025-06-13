46a947d574887ff546ccc40bd716dfa02457319e
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-13 15:44:37 -0300

    Entidad Usuario (Security) creada

---
+++ src/main/java/ar/edu/utn/gestion_inventario/security/model/Usuario.java
@@ -0,0 +1,78 @@
+package ar.edu.utn.gestion_inventario.security.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+
+    @Column(nullable = false)
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id) {
+        this.id = id;
+    }
+
+    public Usuario(String nombre, String apellido, String password, String email, String username) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
