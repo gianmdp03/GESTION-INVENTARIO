77bfddd63bd4c730d2bbcf0c8528c89a600facdb
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-13 18:29:30 -0300

    Transformación a Record de DTO

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/dto/UsuarioLoginDetailDTO.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/usuario/dto/UsuarioLoginDetailDTO.java
@@ -1,31 +1,3 @@
 package ar.edu.utn.gestion_inventario.security.usuario.dto;
 
-public class UsuarioLoginDetailDTO {
-    private String token;
-
-    private Long expiresIn;
-
-    public UsuarioLoginDetailDTO() {
-    }
-
-    public UsuarioLoginDetailDTO(String token, Long expiresIn) {
-        this.token = token;
-        this.expiresIn = expiresIn;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Long getExpiresIn() {
-        return expiresIn;
-    }
-
-    public void setExpiresIn(Long expiresIn) {
-        this.expiresIn = expiresIn;
-    }
-}
+public record UsuarioLoginDetailDTO(String token, Long expiresIn) {}
