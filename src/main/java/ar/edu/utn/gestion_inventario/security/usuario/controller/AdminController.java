fcbfb135401a623e50ef9ae815736a4cce6395c9
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-13 15:30:43 -0300

    Seguridad eliminada
    Se eliminó la implementación de JWT que no funciona

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/controller/AdminController.java
+++
@@ -1,4 +0,0 @@
-package ar.edu.utn.gestion_inventario.security.usuario.controller;
-
-public class AdminController {
-}
--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/dto/UsuarioDetailDTO.java
+++
@@ -1,5 +0,0 @@
-package ar.edu.utn.gestion_inventario.security.usuario.dto;
-
-import java.util.List;
-
-public record UsuarioDetailDTO(String username, List<String> roles){}
