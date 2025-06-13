8463314496ed8a56d7f8e8077d0fc2a8373e2e76
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-13 16:41:00 -0300

    DTOs creados

---
+++ src/main/java/ar/edu/utn/gestion_inventario/security/dto/UsuarioLoginRequestDTO.java
@@ -0,0 +1,35 @@
+package ar.edu.utn.gestion_inventario.security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UsuarioLoginRequestDTO {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    public UsuarioLoginRequestDTO() {
+    }
+
+    public UsuarioLoginRequestDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/ar/edu/utn/gestion_inventario/security/dto/UsuarioRegisterRequestDTO.java
@@ -0,0 +1,59 @@
+package ar.edu.utn.gestion_inventario.security.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UsuarioRegisterRequestDTO {
+    @NotBlank
+    private String nombre;
+
+    @NotBlank
+    private String apellido;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    public UsuarioRegisterRequestDTO() {
+    }
+
+    public UsuarioRegisterRequestDTO(String nombre, String apellido, String username, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
