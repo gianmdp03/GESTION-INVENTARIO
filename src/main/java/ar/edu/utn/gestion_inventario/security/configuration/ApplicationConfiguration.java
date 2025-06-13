3b6f41b28e9445cbe1bd7068e3c54f3357294e8e
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-13 16:20:58 -0300

    ApplicationConfiguration creado (security)

---
+++ src/main/java/ar/edu/utn/gestion_inventario/security/configuration/ApplicationConfiguration.java
@@ -0,0 +1,49 @@
+package ar.edu.utn.gestion_inventario.security.configuration;
+
+import ar.edu.utn.gestion_inventario.exception.NotFoundException;
+import ar.edu.utn.gestion_inventario.security.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService()
+    {
+        return username -> usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new NotFoundException("El nombre de usuario no existe"));
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
+    {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(UserDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+}
