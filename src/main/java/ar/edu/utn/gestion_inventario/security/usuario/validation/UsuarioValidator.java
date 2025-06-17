1f19c64a3c3e7ab1e7aa8e960412e0262f6e674f
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-06-16 22:57:36 -0300

    Metodo para comprobar si el nombre de usuario existe creado

--- src/main/java/ar/edu/utn/gestion_inventario/security/usuario/validation/UsuarioValidator.java
+++ src/main/java/ar/edu/utn/gestion_inventario/security/usuario/validation/UsuarioValidator.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.gestion_inventario.security.usuario.validation;
 
 import ar.edu.utn.gestion_inventario.exception.ConflictException;
+import ar.edu.utn.gestion_inventario.exception.NotFoundException;
 import ar.edu.utn.gestion_inventario.security.usuario.repository.UsuarioRepository;
 
 public class UsuarioValidator {
@@ -11,4 +12,13 @@ public class UsuarioValidator {
             throw new ConflictException("El nombre de usuario ingresado ya existe");
         }
     }
+
+    public static void comprobarUsername(String username, UsuarioRepository usuarioRepository)
+    {
+        if(!(usuarioRepository.existsByUsername(username)))
+        {
+            throw new NotFoundException("El nombre de usuario no existe");
+        }
+    }
+
 }
